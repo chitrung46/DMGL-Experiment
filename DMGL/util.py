5a5b0bfea209bf2e74b47753439075f90f15480b
kiet <chungkiet31@gmail.com>
2025-07-15 11:26:39 +0700

    DC 6 7

--- DMGL/util.py
+++ DMGL/util.py
@@ -259,7 +259,7 @@ def load_dataset(dataset, batch_size, valid_batch_size= None, test_batch_size=No
     # Data format
     for category in ['train', 'val', 'test']:
         if dataset == 'nyc-bike' or dataset =='nyc-taxi' or dataset =='pems03' \
-        or dataset == 'bos-bike' or dataset == 'bay-bike' or dataset == 'dc-bike' or dataset == 'dc-taxi' or dataset == 'chi-taxi' or dataset== 'dc1-taxi' or dataset == 'dc5-taxi' or dataset == 'dc6-taxi or dataset == 'dc7-taxi':
+        or dataset == 'bos-bike' or dataset == 'bay-bike' or dataset == 'dc-bike' or dataset == 'dc-taxi' or dataset == 'chi-taxi' or dataset== 'dc1-taxi' or dataset == 'dc5-taxi' or dataset == 'dc6-taxi' or dataset == 'dc7-taxi':
             #print('load_dataset : nyc transform'+"!"*30)
             data['x_' + category] = scaler.transform(data['x_' + category])            
         else:
